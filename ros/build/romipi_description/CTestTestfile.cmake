9dcfc3e8332c91d00257f21b32f0a1e1e7241c0d
Arturs Elksnis <arturs@elksnis.co.uk>
2022-01-21 14:14:48 +0000

    Fixed build dependency for turtlebot3_msgs. Catkin_make can now build romipi_bringup

--- ros/build/romipi_description/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/student5/robotics/Florence/ros/src/romipi_description
-# Build directory: /home/student5/robotics/Florence/ros/build/romipi_description
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
